07e0c53497103121ce8f0cfd2caa088069fc0734
PhoenixAceVFX <phoenixacevfx@trigon.systems>
2024-12-21 00:14:56 -0500

    Update guns.lol.sh

--- guns.lol.sh
+++ guns.lol.sh
@@ -12,10 +12,7 @@
 # ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
 # ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓████████▓▒░▒▓███████▓▒░  
 # Script by PhoenixAceVFX
-# Licensed under BSD-2
-# You are allowed to include this script in your dotfiles
-# You are allowed to use this script on your file hosts
-# Crediting is required as is the same licensing
+# Licensed under GPL-2.0
 
 #!/bin/bash -e
 
